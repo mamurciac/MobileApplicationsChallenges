d4ba14f4c3ac02a0abdb6479af1a42eaa453e308
Michael Andrés <mamurciac@unal.edu.co>
2020-02-15 21:53:24 -0500

    Images instead of Buttons [Bug Found: Double Gamble]

--- Challenge4_TicTacToe/app/src/main/java/mamurciac/unal/edu/co/challenge4_tictactoe/TicTacToeActivity.java
+++ Challenge4_TicTacToe/app/src/main/java/mamurciac/unal/edu/co/challenge4_tictactoe/TicTacToeActivity.java
@@ -2,7 +2,6 @@ package mamurciac.unal.edu.co.challenge4_tictactoe;
 
 import android.app.Dialog;
 import android.content.*;
-import android.graphics.Color;
 import androidx.appcompat.app.*;
 import android.os.Bundle;
 import android.view.*;
@@ -12,78 +11,66 @@ import java.lang.reflect.*;
 public class TicTacToeActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
     //It represents the game's internal state
     private TicTacToeGame ticTacToeGame;
-    //The buttons make up the board
-    private Button boardButtons[];
+    private BoardView gameBoardView;
+
     //Text displayed as game's information (Turn and winner's game)
     private TextView infoGame, infoNumberHumanWins, infoNumberAndroidWins, infoNumberTies;
 
     //This variables allow to control the game's statistics (Number of games and wins per player)
+    private char playerTurn;
     private int numberGame = 1, numberHumanWins = 0, numberAndroidWins = 0, numberTies = 0;
+    private boolean gameOver;
 
     //Menu options
     static final int dialogDifficultySuccessId = 0, dialogDifficultyFailureId = 1, dialogAboutGameId = 2, dialogQuitId = 3;
     private static String popupConstant = "mPopup", popupForceShowIcon = "setForceShowIcon";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tic_tac_toe);
 
-        boardButtons = new Button[TicTacToeGame.numberSpots];
-        boardButtons[0] = findViewById(R.id.spot_one);
-        boardButtons[1] = findViewById(R.id.spot_two);
-        boardButtons[2] = findViewById(R.id.spot_three);
-        boardButtons[3] = findViewById(R.id.spot_four);
-        boardButtons[4] = findViewById(R.id.spot_five);
-        boardButtons[5] = findViewById(R.id.spot_six);
-        boardButtons[6] = findViewById(R.id.spot_seven);
-        boardButtons[7] = findViewById(R.id.spot_eight);
-        boardButtons[8] = findViewById(R.id.spot_nine);
         infoGame = findViewById(R.id.game_information);
         infoNumberHumanWins = findViewById(R.id.number_human_wins);
         infoNumberAndroidWins = findViewById(R.id.number_android_wins);
         infoNumberTies = findViewById(R.id.number_ties);
         ticTacToeGame = new TicTacToeGame();
+
+        gameOver = false;
+        gameBoardView = findViewById(R.id.game_board);
+        gameBoardView.setGame(ticTacToeGame);
+        //It listens for touches on the board
+        gameBoardView.setOnTouchListener(touchListener);
         startNewGame();
     }
 
     private void startNewGame(){
         ticTacToeGame.clearBoard();
-
-        //It resets all buttons
-        for(int spot = 0; spot < boardButtons.length; spot++) {
-            boardButtons[spot].setText("");
-            boardButtons[spot].setEnabled(true);
-            boardButtons[spot].setOnClickListener(new ButtonClickListener(spot));
-        }
+        gameBoardView.invalidate(); //It redraws the board
+        gameOver = false;
 
         //According to the turn, human or computer player goes first
         if(numberGame % 2 == 1){
             infoGame.setText(R.string.first_turn_human);
         }else{
+            playerTurn = TicTacToeGame.computerPlayer;
             infoGame.setText(R.string.first_turn_computer);
             int move = ticTacToeGame.getComputerMove();
             ticTacToeGame.setMove(TicTacToeGame.computerPlayer, move);
-            boardButtons[move].setEnabled(false);
-            boardButtons[move].setText(String.valueOf(TicTacToeGame.computerPlayer));
-            boardButtons[move].setTextColor(Color.rgb(200, 0, 0));
             infoGame.setText(R.string.turn_human);
         }
+        playerTurn = TicTacToeGame.humanPlayer;
         infoNumberHumanWins.setText("Human Wins: " + numberHumanWins);
         infoNumberAndroidWins.setText("Android Wins: " + numberAndroidWins);
         infoNumberTies.setText("Ties: " + numberTies);
     }
 
-    private void setMove(char player, int location){
-        ticTacToeGame.setMove(player, location);
-        boardButtons[location].setEnabled(false);
-        boardButtons[location].setText(String.valueOf(player));
-
-        if(player == TicTacToeGame.humanPlayer){
-            boardButtons[location].setTextColor(Color.rgb(0,200,0));
-        }else{
-            boardButtons[location].setTextColor(Color.rgb(200,0,0));
+    private boolean setMove(char player, int location){
+        if(ticTacToeGame.setMove(player, location) == true){
+            gameBoardView.invalidate(); //It redraws the board
+            return true;
         }
+        return false;
     }
 
     @Override
@@ -194,50 +181,66 @@ public class TicTacToeActivity extends AppCompatActivity implements PopupMenu.On
         popup.show();
     }
 
-    //It handles clicks on the gameboard buttons
-    private class ButtonClickListener implements View.OnClickListener{
-        int location;
-        public ButtonClickListener(int location){
-            this.location = location;
-        }
+    //It listens for touches on the board
+    private View.OnTouchListener touchListener = new View.OnTouchListener(){
+        @Override
+        public boolean onTouch(View view, MotionEvent event){
+            if(playerTurn == TicTacToeGame.humanPlayer){
+                //It determines which cell was touched
+                int column = (int) event.getX() / gameBoardView.getBoardCellWidth();
+                int row = (int) event.getY() / gameBoardView.getBoardCellHeight();
+                int spot = row * 3 + column;
 
-        public void onClick(View view){
-            if(boardButtons[location].isEnabled()){
-                setMove(TicTacToeGame.humanPlayer, location);
-                //If there isn't winner yet, then it lets the computer make a move
-                int winner = ticTacToeGame.checkForWinner();
-                if(winner == TicTacToeGame.gameNotFinished){
-                    infoGame.setText(R.string.turn_computer);
-                    int move = ticTacToeGame.getComputerMove();
-                    setMove(TicTacToeGame.computerPlayer, move);
-                    winner = ticTacToeGame.checkForWinner();
-                }
+                if(gameOver == false && setMove(TicTacToeGame.humanPlayer, spot) == true){
+                    //If there isn't winner yet, then it lets the computer make a move
+                    int winner = ticTacToeGame.checkForWinner();
+                    if(winner == TicTacToeGame.gameNotFinished){
+                        infoGame.setText(R.string.turn_computer);
+                        playerTurn = TicTacToeGame.computerPlayer;
+                        int move = ticTacToeGame.getComputerMove();
+                        setMove(TicTacToeGame.computerPlayer, move);
+                        winner = ticTacToeGame.checkForWinner();
 
-                if(winner == TicTacToeGame.gameNotFinished){
-                    infoGame.setText(R.string.turn_human);
-                }else if(winner == TicTacToeGame.gameTied){
-                    infoGame.setText(R.string.result_tie);
-                    for(int spot = 0; spot < boardButtons.length; spot++){
-                        boardButtons[spot].setEnabled(false);
+                        if(winner == TicTacToeGame.gameNotFinished){
+                            infoGame.setText(R.string.turn_human);
+                            playerTurn = TicTacToeGame.humanPlayer;
+                        }else if(winner == TicTacToeGame.gameTied){
+                            infoGame.setText(R.string.result_tie);
+                            gameOver = true;
+                            numberTies++;
+                            infoNumberTies.setText("Ties: " + numberTies);
+                        }else if(winner == TicTacToeGame.gameWithHumanWinner){
+                            infoGame.setText(R.string.result_human_wins);
+                            gameOver = true;
+                            numberHumanWins++;
+                            infoNumberHumanWins.setText("Human Wins: " + numberHumanWins);
+                        }else{
+                            infoGame.setText(R.string.result_computer_wins);
+                            gameOver = true;
+                            numberAndroidWins++;
+                            infoNumberAndroidWins.setText("Android Wins: " + numberAndroidWins);
+                        }
+                    }else if(winner == TicTacToeGame.gameTied){
+                        infoGame.setText(R.string.result_tie);
+                        gameOver = true;
+                        numberTies++;
+                        infoNumberTies.setText("Ties: " + numberTies);
+                    }else if(winner == TicTacToeGame.gameWithHumanWinner){
+                        infoGame.setText(R.string.result_human_wins);
+                        gameOver = true;
+                        numberHumanWins++;
+                        infoNumberHumanWins.setText("Human Wins: " + numberHumanWins);
+                    }else{
+                        infoGame.setText(R.string.result_computer_wins);
+                        gameOver = true;
+                        numberAndroidWins++;
+                        infoNumberAndroidWins.setText("Android Wins: " + numberAndroidWins);
                     }
-                    numberTies++;
-                    infoNumberTies.setText("Ties: " + numberTies);
-                }else if(winner == TicTacToeGame.gameWithHumanWinner){
-                    infoGame.setText(R.string.result_human_wins);
-                    for(int spot = 0; spot < boardButtons.length; spot++){
-                        boardButtons[spot].setEnabled(false);
-                    }
-                    numberHumanWins++;
-                    infoNumberHumanWins.setText("Human Wins: " + numberHumanWins);
-                }else{
-                    infoGame.setText(R.string.result_computer_wins);
-                    for(int spot = 0; spot < boardButtons.length; spot++){
-                        boardButtons[spot].setEnabled(false);
-                    }
-                    numberAndroidWins++;
-                    infoNumberAndroidWins.setText("Android Wins: " + numberAndroidWins);
+                    return true;
                 }
             }
+            //So we aren't notified of continued events when finger is moved
+            return false;
         }
-    }
+    };
 }
